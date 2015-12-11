59e7a21aaa8c5103ed1ddd9ff67e5db778205241
Chris Vasquez <christv@aweber.com>
2015-12-11 17:19:26 -0500

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zeneditorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zeneditorApp
+ */
+angular.module('zeneditorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zeneditorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zeneditorApp
+ */
+angular.module('zeneditorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
