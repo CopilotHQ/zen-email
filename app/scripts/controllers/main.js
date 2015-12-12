c156d6bdf65e48681b4f2c66f8ea0516d648e1d3
Chris Vasquez <christv@aweber.com>
2015-12-11 22:53:14 -0500

    Start setting up angular controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,13 @@
  * Controller of the zeneditorApp
  */
 angular.module('zeneditorApp')
-  .controller('MainCtrl', function () {
+  .controller('SlideCtrl', ['$scope', function ($scope) {
     var header = angular.element( document.querySelector( '.header span' ) );
     var buttons = angular.element( document.querySelector( '.buttons span' ) );
+
+    $scope.headline = 'Hello there.';
 
     header.fadeIn(5000);
     buttons.delay( 500 ).fadeIn( 4000 );
     console.log(header);
-  });
+  }]);
